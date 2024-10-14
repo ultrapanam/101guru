44706f3721c4260097ee0cf24bf60c027d6e67ef
ultrapanam2 <ultrapanam2@gmail.com>
2024-10-14 20:02:51 +0300

    Added pragraph block. With borders and disclaimer option

--- components/modules/module_text-block.php
+++ components/modules/module_text-block.php
@@ -3,3 +3,22 @@
 /**
  * Module -- Text Block
  */
+
+if (isset($module)) {
+    $heading = (isset($module['heading'])) ? $module['heading'] : false;
+    $paragraph = (isset($module['paragraph'])) ? $module['paragraph'] : false;
+    $border = (isset($module['border'])) ? $module['border'] : false;
+?>
+
+<div class="section">
+    <div class="block-container <?php echo $border; ?>">
+        <div class="heading-text">
+            <h2><?php echo $heading; ?></h2>
+        </div>
+        <div class="paragraph">
+            <?php echo $paragraph; ?>
+        </div>
+    </div>
+</div>
+
+<?php }?>
