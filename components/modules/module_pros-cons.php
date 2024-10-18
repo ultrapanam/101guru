cb5a53fb6aa8d725a6aa228b34ab4856a41fef6d
ultrapanam2 <ultrapanam2@gmail.com>
2024-10-18 14:53:28 +0300

    added pros-cons block

--- components/modules/module_pros-cons.php
+++ components/modules/module_pros-cons.php
@@ -3,3 +3,33 @@
 /**
  * Module -- Pros and Cons
  */
+
+ if (isset($module)) {
+    $pros_cons_heading = (isset($module['pros-cons-heading'])) ? $module['pros-cons-heading'] : false;
+    $pros = (isset($module['pros'])) ? $module['pros'] : false;
+    $cons = (isset($module['cons'])) ? $module['cons'] : false;
+
+?>
+<div class="section pros-cons">
+    <div class="block-container">
+        <h2><?php echo $pros_cons_heading; ?></h2>
+        <div class="pros-cons-wrapper">
+            <div class="pros blue-border">
+                <ul class="proscons-list">
+                    <?php foreach ($pros as $item) { ?>
+                        <li><?php echo $item['item']; ?></li>
+                    <?php } ?>
+                </ul>
+            </div>
+            <div class="cons blue-border">
+                <ul class="proscons-list">
+                    <?php foreach ($cons as $item) { ?>
+                        <li><?php echo $item['item']; ?></li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php } ?>
